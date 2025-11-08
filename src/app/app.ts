34a4d37c5b6a827b74e604d8c3a3bce3024ea27d
william-wv <wwillveiga@hotmail.com>
2025-11-08 16:48:02 -0300

    merges routing & modal fixed

--- src/app/app.ts
+++ src/app/app.ts
@@ -19,31 +19,24 @@ import { Card } from './components/card/card';
   // 2. Array de 'imports' COMBINADO com todos os itens
   imports: [
     RouterOutlet,
-    CardMainComponent,
-    AppButtonComponent,
-    AppInputComponent,
     Sidebar,
     Header,
     Modal,
     Card
   ],
   
-  // 3. Arquivos de template e estilo (escolhi um, você tinha duplicados)
   templateUrl: './app.html', 
   styleUrl: './app.component.scss',
 })
 
-// 4. APENAS UMA declaração de classe
-export class App { // Seu código usou 'App', não 'AppComponent'
+export class App { 
 
-  // 5. Propriedades da classe COMBINADAS (removi a duplicata 'title')
   protected readonly title = signal('UTF-Check');
   password: any;
   username: any;
   isModalVisible = false;
   modalTitle = '';
 
-  // 6. Métodos da classe
   openModal(content: string) {
     this.modalTitle = content;
     this.isModalVisible = true;
