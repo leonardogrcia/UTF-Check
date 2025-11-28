c3a5b6b5e2fdd8491b27c4d2aed6336c71a28496
William <wwillveiga@hotmail.com>
2025-11-27 23:40:52 -0300

    fix auth

--- src/app/components/input/input.ts
+++ src/app/components/input/input.ts
@@ -1,27 +1,24 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   templateUrl: './input.html',
-  styleUrls: ['./input.scss']
+  styleUrls: ['./input.scss'],
 })
 export class AppInputComponent {
-  @Input() label: string = '';
-  @Input() icon: string = '';
+  @Input() label = '';
+  @Input() icon = '';
   @Input() type: string = 'text';
 
-  // Two-way binding
   @Input() value: string = '';
   @Output() valueChange = new EventEmitter<string>();
 
-  onValueChange(newValue: string) {
+  onInput(event: Event) {
+    const newValue = (event.target as HTMLInputElement).value;
     this.value = newValue;
     this.valueChange.emit(newValue);
   }
 }
-export { Input };
-
